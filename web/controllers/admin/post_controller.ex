165edb5b89308f3e2cfe894e7214dfaa41d003dd
Trevyn Loris <trevyn.loris@gmail.com>
2015-06-15 22:52:37 -0500

    Redirect to admin path after admin edits.

--- web/controllers/admin/post_controller.ex
+++ web/controllers/admin/post_controller.ex
@@ -33,7 +33,7 @@ defmodule Blog.Admin.PostController do
 
       conn
       |> put_flash(:info, "Post created successfully.")
-      |> redirect(to: post_path(conn, :index))
+      |> redirect(to: admin_post_path(conn, :index))
     else
       render(conn, "new.html", changeset: changeset)
     end
@@ -59,7 +59,7 @@ defmodule Blog.Admin.PostController do
 
       conn
       |> put_flash(:info, "Post updated successfully.")
-      |> redirect(to: post_path(conn, :index))
+      |> redirect(to: admin_post_path(conn, :index))
     else
       render(conn, "edit.html", post: post, changeset: changeset)
     end
@@ -71,6 +71,6 @@ defmodule Blog.Admin.PostController do
 
     conn
     |> put_flash(:info, "Post deleted successfully.")
-    |> redirect(to: post_path(conn, :index))
+    |> redirect(to: admin_post_path(conn, :index))
   end
 end
